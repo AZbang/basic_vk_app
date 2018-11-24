b9e461f4d08a278ba5104b0b6e336511b8b06a51
AZbang <azbango5@gmail.com>
2018-11-24 19:19:47 +0300

    Updates

---
+++ precache-manifest.a82ed774dda5ece3af5b89218f4d223a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96c121ed8007ce9d8510",
+    "url": "/vk_app/static/js/runtime~main.96c121ed.js"
+  },
+  {
+    "revision": "b239d8fe5abb569fd296",
+    "url": "/vk_app/static/js/main.b239d8fe.chunk.js"
+  },
+  {
+    "revision": "7f428e70c6cfa6612014",
+    "url": "/vk_app/static/js/1.7f428e70.chunk.js"
+  },
+  {
+    "revision": "b239d8fe5abb569fd296",
+    "url": "/vk_app/static/css/main.b0b47fa0.chunk.css"
+  },
+  {
+    "revision": "7f428e70c6cfa6612014",
+    "url": "/vk_app/static/css/1.faae2728.chunk.css"
+  },
+  {
+    "revision": "fe68345c3edfa0829932ffd19fa33b30",
+    "url": "/vk_app/index.html"
+  }
+];
